0d6729dfc0db76a785e36b8384962c3c40f12920
luis felipe amador bravo <luigifelipe333@gmail.com>
2026-06-23 18:16:04 -0600

    Generacion automatica de codigo de producto

--- backend/app/Http/Requests/StoreProductRequest.php
+++ backend/app/Http/Requests/StoreProductRequest.php
@@ -22,10 +22,9 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'code' => 'required|string|max:50|unique:products',
             'name' => 'required|string|max:255',
             'brand' => 'required|string|max:255',
-            'price' => 'required|numeric|min:0',
+            'price' => 'required|numeric|min:0|max:999',
         ];
     }
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -21,4 +21,28 @@ class Product extends Model
     protected $casts = [
         'price' => 'float',
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(function ($product) {
+
+            $lastProduct = self::orderBy(   //Obtiene el ultimo producto por numero de codigo
+                'code_number',              //Apesar de la duplicacion de datos del codigo, se hace para evitar problemas de ordenamiento en el futuro
+                'desc'                      
+            )->first();
+
+            $nextNumber = 1;
+
+            if ($lastProduct !== null) {
+                $nextNumber = $lastProduct->code_number + 1;
+            }
+
+            $product->code_number = $nextNumber;
+
+            $product->code = sprintf(
+                'P%04d',
+                $nextNumber
+            );
+        });
+    }
 }
