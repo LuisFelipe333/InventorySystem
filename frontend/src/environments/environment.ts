98cde9aabc33d6264ac6e62adcc3b0779f085692
luis felipe amador bravo <luigifelipe333@gmail.com>
2026-06-25 21:30:07 -0600

    Se agrega ambiente de produccion

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
-    storageUrl: 'http://localhost:8000/storage'
+    apiUrl: 'https://p01--inventory-backend--6lgwb72f7vv9.code.run/api',
+    storageUrl: 'https://p01--inventory-backend--6lgwb72f7vv9.code.run/storage'
 };
